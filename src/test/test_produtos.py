91fb669af0c7ebc48cc5ee620c3af96ca6c98c14
Mateus Lourenço <mateuslourenco55@outlook.com>
2022-03-07 03:20:08 -0300

    adicionado teste produto inexistente

--- src/test/test_produtos.py
+++ src/test/test_produtos.py
@@ -13,3 +13,13 @@ def resp():
 
 def test_listar_produtos(resp):
     assert resp.status_code == 200
+
+
+@pytest.fixture
+def resp_produto_inexistente():
+    return client.get('/produtos/1234')
+
+
+def test_obter_produto_inexistente(resp_produto_inexistente):
+    assert resp_produto_inexistente.status_code == 404, resp_produto_inexistente.text
+    assert resp_produto_inexistente.json() == {'detail': 'Produto não localizado'}
